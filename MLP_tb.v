1a1549762a4fa038e94a0c8270dce1c8d291c393
HesamAsad <asadzadeh.hesam@ut.ac.ir>
2022-01-20 20:29:00 +0330

    testbench updated

--- MLP_tb.v
+++ MLP_tb.v
@@ -1,22 +1,25 @@
 module MLP_tb;
-    reg clk = 1'b0, rst=1'b1, start = 1'b0;
+    reg clk = 1'b0, rst=1'b1, start = 1'b0, mem_read=1'b1;
     reg[9:0] test_num;
-    wire[3:0] out;
+    wire[3:0] out, label;
     wire done;
+    label_mem u6(test_num, mem_read, label);
     MLP u(
     clk, rst, start, test_num, out, done
     );
-    integer i;
+    integer i, cnt=0;
     always #50 clk = ~clk;
     initial begin
-        for(i=0; i<4; i=i+1) begin
+        for(i=0; i<100; i=i+1) begin
             rst = 1'b1;
             start = 1'b0;
-            test_num = i;
+            test_num = $urandom%750;
             #75 rst = 1'b0;
             start = 1'b1;
             #100 start = 1'b0;
             #16000;
+            if(label == out)
+                cnt = cnt+1;
         end
         $stop; 
     end
